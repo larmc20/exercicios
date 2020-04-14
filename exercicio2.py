c741e9744d32058f3270ab8003b93dd37d380e7b
Luiz Arthur Carvalho Exm <larmc20@yahoo.com.br>
2020-04-14 15:12:42 -0300

    Outra maneira de montar o exercício

--- exercicio2.py
+++ exercicio2.py
@@ -1,17 +1,16 @@
 """Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela 
 o nome do escolhido."""
-
 #imports
 import random
 
-n1 = str(input("Aluno 1:\n"))
-n2 = str(input("Aluno 2:\n"))
-n3 = str(input("Aluno 3:\n"))
-n4 = str(input("Aluno 4:\n"))
+def main():
+    alunos = []
+    for i in range(1,5):
+        alunos.append(str(input(f"Aluno {i}:\n")))
+    print(type(alunos))
 
-alunos = [n1, n2, n3, n4]
-print(type(alunos))
+    print("O aluno escolhido foi {}".format(random.choice(alunos)))
 
-print("O aluno escolhido foi {}".format(random.choice(alunos)))
-
+if __name__ == "__main__":
+    main()
