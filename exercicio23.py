d334ca51139cd62f2ef7aa1de4b0d7e1265aea0d
Luiz Arthur Carvalho Exm <larmc20@yahoo.com.br>
2020-04-15 10:34:01 -0300

    Exercicio 23 finalizado

--- exercicio23.py
+++ exercicio23.py
@@ -1,5 +1,22 @@
 #imports
 from random import randint
+numero = randint(0,9999)
 
-numero = randint(1,999)
+letras = [i for i in str(numero)]
 
+if len(letras) > 3:
+    print("Milhares: " + letras[0])
+    print("Centenas: " + letras[1])    
+    print("Dezenas: " + letras[2])
+    print("Unidades: " + letras[3])
+elif len(letras) > 2:
+    print("Centenas: " + letras[0])    
+    print("Dezenas: " + letras[1])
+    print("Unidades: " + letras[2])
+elif len(letras) > 1:
+    print("Dezenas: " + letras[0])
+    print("Unidades: " + letras[1]) 
+else:
+    print("Unidades: " + letras[0]) 
+
+print("O número é: " + str(numero))
